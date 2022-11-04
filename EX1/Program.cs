6deb646d9eb7807d16370a44223757070b640f93
Zukhra <zuhruschka@mail.ru>
2022-11-04 20:06:21 +0400

    1ex

---
+++ EX1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход 
+//пятизначное число и проверяет, является ли оно палиндромом//
+Console.WriteLine("Введите пятизначное число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+
+ void CheckingNumber(number);
+
+ if (Number[0]==Number[4]||Number[1]==Number [3])
+ Console.WriteLine("Чиcло является палиндромом");
+ else 
+ {
+Console.WriteLine("Чиcло не является палиндромом");
+ };
+CheckingNumber (Number);
