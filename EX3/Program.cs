3a71f7146f21da7e622ad978f080017d9b5055d9
Zukhra <zuhruschka@mail.ru>
2022-11-04 20:42:49 +0400

    3ex

--- EX3/Program.cs
+++ EX3/Program.cs
@@ -1,14 +1,11 @@
 ﻿// Напишите программу, которая принимает на вход число (N) 
 //и выдаёт таблицу кубов чисел от 1 до N.//
-void PrintTheThirdDegreeToConsole (int n)
-{
-   for (int=1; i<=1; i++);
-   {
-    Console.WriteLine (i*i*i);
-   }
-}
-
-Console.WriteLine ("Введите число: ");
+Console.WriteLine( "Введите число N" );
 int number = Convert.ToInt32(Console.ReadLine());
-
-PrintTheThirdDegreeToConsole (int);
+Console.WriteLine();
+int count = 1;
+while(count<=number)
+{
+   Console.WriteLine(Math.Pow(count,3));
+    count++;
+}
